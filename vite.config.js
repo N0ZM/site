6ef53156e2a6b94db5df245e1611f6b2dfe4be8c
NZM <nzm@NZMs-MacBook-Pro.local>
2025-09-25 14:48:32 +0900

    0

--- vite.config.js
+++ vite.config.js
@@ -13,8 +13,11 @@ export default defineConfig({
         outDir: 'docs', 
         rollupOptions: {
             input: {
+                '': resolve(__dirname, 'index.html'), 
                 1: resolve(__dirname, '1/index.html'), 
-                2: resolve(__dirname, '2/index.html')
+                2: resolve(__dirname, '2/index.html'), 
+                'agents/1': resolve(__dirname, 'agents/1/index.html'), 
+                'agents/2': resolve(__dirname, 'agents/2/index.html'), 
             }
         }
     }
